c031c8f71e3fb31ed0e1e0707224055b41cc78dc
Andrew28-cyber <andrey.schule@gmail.com>
2022-05-11 19:45:19 +0300

    Adding tape and finishing logic

---
+++ Assets/MyGame/Scripts/GameManagerScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public void HoverEntered(string a)
+    {
+        Debug.Log(a);
+    }
+
+    public void HoverExit(string a)
+    {
+        Debug.Log(a);
+    }
+
+    public void SelectEntered(string a)
+    {
+        Debug.Log(a);
+    }
+
+    public void SelectExit(string a)
+    {
+        Debug.Log(a);
+    }
+}
